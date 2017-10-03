328468fab49573aee0746414e37a999e775ac095
uyth <uyth95@gmail.com>
2017-10-04 00:38:41 +0200

    [Move Player] can move player, missing ladder and win condition

--- game.js
+++ game.js
@@ -10,47 +10,53 @@ $(document).ready(function() {
         dice: 0,
         gameOver: false,
         players: [],
+
+        // inits all
         init: function e() {
           game.initBoard();
           game.initPlayers();
         },
-        initBoard: function e() {
-            // add all cells to board
-            for (var i = 0; i < 90; i++) {
-                var cell = $("#index-" + i);
-                cell.css("border", "3px solid black");
-                var row = cell.css("grid-row").split("/")[0];
-                var col = cell.css("grid-column").split("/")[0];
-                game.board.push(new Cell(i, row, col));
-            }
-        },
-        initPlayers: function e() {
-            game.players = [];
-            playerCount = $('input[name="playerInput"]').val();
-            for (var i = 0; i < playerCount; i++) {
-                // add player with inital start at cell 0
-                game.players.push(new Player(i,game.board[0]));
-                console.log(i);
-            }
-            console.log(game.players);
-        },
+            initBoard: function e() {
+                // add all cells to board
+                for (var i = 0; i < 90; i++) {
+                    var cell = $("#index-" + i);
+                    cell.css("border", "3px solid black");
+                    var row = cell.css("grid-row").split("/")[0];
+                    var col = cell.css("grid-column").split("/")[0];
+                    game.board.push(new Cell(i, row, col));
+                }
+            },
+            initPlayers: function e() {
+                game.players = [];
+                playerCount = $('input[name="playerInput"]').val();
+                for (var i = 0; i < playerCount; i++) {
+                    // add player with inital start at cell 0
+                    game.players.push(new Player(i,game.board[0]));
+                    console.log(i);
+                }
+                game.updateView();
+            },
+
+        // updates all views
         updateView: function e() {
             document.getElementById("dice").innerHTML = '' + game.dice;
             document.getElementById("currentPlayer").innerHTML = '' + (game.currentPlayer + 1);
             game.updatePlayerPosition();
         },
-        updatePlayerPosition: function e() {
-            game.players.forEach(function(player) {
-                var element = document.getElementById("player-" + player.playerIndex);
-                var cell = player.currentCell;
-                element.className = 'player ' + "row-" + (cell.row - 1) + " col-" + (cell.col-1);
-            });
-        },
+            updatePlayerPosition: function e() {
+                game.players.forEach(function(player) {
+                    var element = document.getElementById("player-" + player.index);
+                    var cell = player.currentCell;
+                    element.className = 'player ' + "row-" + (cell.row - 1) + " col-" + (cell.col-1);
+                });
+            },
+
         rollDice: function e() {
             // roll a dice between 1 and 6
             game.dice = Math.floor(Math.random()*(7 - 1) + 1);
             game.updateView();
-            game.players[game.currentPlayer].actions.move();
+            game.movePlayer();
+            //if ()
             game.nextTurn();
         },
         nextTurn: function e() {
@@ -63,7 +69,31 @@ $(document).ready(function() {
         },
         ladders: [(3,17), (8,10), (15,44), (22,5),
             (39,56), (49,75), (62,45), (64,19),
-            (65,73), (80,12), (87,79)]
+            (65,73), (80,12), (87,79)],
+        movePlayer: function e() {
+            var player = game.players[game.currentPlayer];
+            console.log(player);
+            player.currentCell.players.delete(player);
+
+            // go to next cell
+            var indexBoard = player.currentCell.index;
+
+            // if player passes last cell
+            if (indexBoard + game.dice >= 90) {
+                var steps = indexBoard + game.dice - 89;
+                indexBoard = 89 - steps;
+            } else {
+                indexBoard += game.dice;
+            }
+
+            player.currentCell = game.board[indexBoard];
+            //  if ladder on cell go to end-of-ladder-cell
+            if (player.currentCell.ladder) {
+                //player.currentCell.
+            }
+
+            player.currentCell.players.add(player);
+        }
     };
 
     // init Cell
@@ -72,35 +102,23 @@ $(document).ready(function() {
         this.row = row;
         this.col = col;
         this.ladder = false;
-        this.players = [];
+        this.players = new Set();
     };
 
     // init Player
     var Player = function(index, cell) {
-        this.playerIndex = index;
+        this.index = index;
         this.currentCell = cell;
-        this.currentCell.players.push(this);
-        this.actions = {
-            move: function e(){
-                var indexBoard = currentCell.index;
-                currentCell.players.pop(this);
-
-                indexBoard += game.dice;
-                currentCell = game.board[indexBoard];
-                currentCell.players.push(this);
-            }
-        }
+        this.currentCell.players.add(this);
     };
 
     game.init();
 
     buttonDice.click(function e() {
-        console.log("dice");
         game.rollDice();
 
     });
     buttonReset.click(function e() {
-        console.log("reset");
         game.init();
 
     });
